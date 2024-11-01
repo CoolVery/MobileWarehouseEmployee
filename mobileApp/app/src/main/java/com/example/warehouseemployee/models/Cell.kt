084295b419c720f7a98dceb7eea9c83528365ccc
CoolVery <gorgn2005@gmail.com>
2024-11-01 09:28:31 +0300

    app: created model Cell

---
+++ mobileApp/app/src/main/java/com/example/warehouseemployee/models/Cell.kt
@@ -0,0 +1,12 @@
+package com.example.warehouseemployee.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Cell (
+    @SerialName("id")
+    val id: Int,
+
+
+)
