f6365ae26fe6e866837d26010af1add5f6daf880
CoolVery <gorgn2005@gmail.com>
2024-11-15 11:14:39 +0300

    app: need to finish InfoTaskLoading.kt

--- mobileApp/app/src/main/java/com/example/warehouseemployee/presentation/screens/infotask/InfoTaskLoading.kt
+++ mobileApp/app/src/main/java/com/example/warehouseemployee/presentation/screens/infotask/InfoTaskLoading.kt
@@ -2,12 +2,18 @@ package com.example.warehouseemployee.presentation.screens.infotask
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -21,6 +27,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -118,51 +125,107 @@ fun InfoTaskLoading(
                 items(
                     cellAndProductList
                 ) { itemList ->
-                    Row (
+                    Box(
                         modifier = Modifier
                             .fillMaxWidth()
-                    ) {
-                        Column (
-                            modifier = Modifier
-                                .weight(1.5f)
-                                .padding(10.dp, 10.dp)
-                                .background(WarehouseEmployeeTheme.colors.background)
-                        ) {
-                            Text(
-                                text = itemList.idProduct.article
-                            )
-                            Text(
-                                text = itemList.idProduct.productName
-                            )
-                            Text(
-                                text = "Кол-во: ${itemList.countProduct}"
-                            )
-                            Text(
-                                text = "Вес: ${itemList.idProduct.weight * itemList.countProduct} кг"
-                            )
-                        }
+                            .fillMaxHeight(0.4f)
 
-                        Image(
-                            modifier = Modifier
-                                .weight(1f),
-                            painter = painterResource(id = R.drawable.arrow),
-                            contentDescription = ""
-                        )
-                        Column (
-                            verticalArrangement = Arrangement.Center,
+                            .background(Color.Green)
+
+                    ) {
+                        Row(
                             modifier = Modifier
                                 .fillMaxSize()
-                                .weight(1.5f)
-                                .background(WarehouseEmployeeTheme.colors.background)
+                                .fillMaxHeight(1f
+                                )
+                                .border(
+                                    width = 3.dp,
+                                    color = Color.Red
+                                )
                         ) {
-                            Text(
-                                text = itemList.idCell.abbreviatedName
-                            )
+
+
+                            Column(
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .fillMaxHeight(1f)
+                                    .border(
+                                        width = 3.dp,
+                                        color = Color.Red
+                                    )
+                            ) {
+
+
+                                Text(
+                                    text = itemList.idProduct.article,
+                                    style = WarehouseEmployeeTheme.typography.secondText,
+                                    color = WarehouseEmployeeTheme.colors.text_color_second_element
+                                )
+                                Text(
+                                    text = itemList.idProduct.productName,
+                                    style = WarehouseEmployeeTheme.typography.secondText,
+                                    color = WarehouseEmployeeTheme.colors.text_color_second_element
+                                )
+                                Text(
+                                    text = "Кол-во: ${itemList.countProduct}",
+                                    style = WarehouseEmployeeTheme.typography.secondText,
+                                    color = WarehouseEmployeeTheme.colors.text_color_second_element
+                                )
+                                Text(
+                                    text = "Вес: ${itemList.idProduct.weight * itemList.countProduct} кг",
+                                    style = WarehouseEmployeeTheme.typography.secondText,
+                                    color = WarehouseEmployeeTheme.colors.text_color_second_element
+                                )
+                            }
+                            Column(
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .fillMaxHeight()
+                                    .border(
+                                        width = 3.dp,
+                                        color = Color.Red
+                                    )
+                            ) {
+                                Box(
+                                    modifier = Modifier
+                                        .border(
+
+                                            width = 3.dp,
+                                            color = Color.Red
+                                        )
+                                ) {
+                                    Image(
+                                        modifier = Modifier,
+                                        painter = painterResource(id = R.drawable.arrow),
+                                        contentDescription = ""
+                                    )
+                                }
+
+                                Box(
+                                    modifier = Modifier
+                                        .border(
+                                            width = 3.dp,
+                                            color = Color.Red
+                                        )
+                                        .fillMaxSize()
+                                        .background(WarehouseEmployeeTheme.colors.background)
+                                ) {
+                                    Text(
+                                        text = itemList.idCell.abbreviatedName,
+                                        style = WarehouseEmployeeTheme.typography.secondText,
+                                        color = WarehouseEmployeeTheme.colors.text_color_second_element,
+                                        modifier = Modifier
+
+
+                                    )
+                                }
+                            }
                         }
                     }
                 }
-            }
+
         }
-        LazyColumn { }
     }
+    LazyColumn { }
+}
 }
